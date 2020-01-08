144972fb611d335121595864d7533b9b5971c241
thackerhelik <thackerhelik@users.noreply.github.com>
2020-01-09 01:44:54 +0530

    Update gta5.py

--- gta5.py
+++ gta5.py
@@ -13,36 +13,39 @@ def process_img(original_image):
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
     return processed_img    
 
-for i in list(range(4))[::-1]:
-    print(i+1)
-    time.sleep(1)
-
-last_time = time.time()
-while(True):
-    screen =np.array(ImageGrab.grab(bbox = (0, 40, 800, 640)))
-    #printscreen_numpy = np.array(printscreen_pil.getdata(), dtype='uint8').reshape((printscreen_pil.size[1], printscreen_pil.size[0], 3))
-    new_screen = process_img(screen)
-
-# =============================================================================
-#     print('down')
-#     PressKey(W)
-#     time.sleep(3)
-#     print('up')
-#     ReleaseKey(W)
-#     
-#     print('down')
-#     PressKey(S)
-#     time.sleep(3)
-#     print('up')
-#     ReleaseKey(S)
-# 
-# =============================================================================
+def main():
+        
+    for i in list(range(4))[::-1]:
+        print(i+1)
+        time.sleep(1)
     
-    print('Loop took {} seconds'.format(time.time()-last_time))
     last_time = time.time()
+    while(True):
+        screen =np.array(ImageGrab.grab(bbox = (0, 40, 800, 640)))
+        #printscreen_numpy = np.array(printscreen_pil.getdata(), dtype='uint8').reshape((printscreen_pil.size[1], printscreen_pil.size[0], 3))
+        new_screen = process_img(screen)
     
-    cv2.imshow('window', new_screen)
-    #cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+        # =============================================================================
+        #     print('down')
+        #     PressKey(W)
+        #     time.sleep(3)
+        #     print('up')
+        #     ReleaseKey(W)
+        #     
+        #     print('down')
+        #     PressKey(S)
+        #     time.sleep(3)
+        #     print('up')
+        #     ReleaseKey(S)
+        # =============================================================================
+        
+        print('Loop took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
+        
+        cv2.imshow('window', new_screen)
+        #cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+        
+main()
